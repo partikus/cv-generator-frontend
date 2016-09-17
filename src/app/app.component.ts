652c5164c4ae14a22caebeeda087ec930500b909
Kamil Szkutnik <kszkutnik@getpodium.co.uk>
2016-09-17 11:00:23 +0200

    Project init.

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+/*
+ * Angular 2 decorators and services
+ */
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.style.css'
+  ],
+  template: `
+    <nav>
+      <span>
+        <a [routerLink]=" ['./'] ">
+          Index
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./home'] ">
+          Home
+        </a>
+      </span>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+
+    <footer>
+      Footer
+    </footer>
+  `
+})
+export class App {
+  angularclassLogo = 'assets/img/angularclass-avatar.png';
+  name = 'Angular 2 Webpack Starter';
+  url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState) {
+
+  }
+
+  ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
